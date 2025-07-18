59f8f35ef78b5136945fdf7f28a4ec0f57f5cdb9
progaurav052 <gpprojects511@gmail.com>
2025-07-18 17:17:11 +0530

    Created an Design of the website in readme.md

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div className="App">
-      <h1 className="font-bold text-2xl bg-blue-200">Welcome to Netflix-gpt</h1>
+      <h1 className="font-bold text-2xl bg-blue-200 shadow-lg m-2 p-2 rounded-lg">Welcome to Netflix-gpt</h1>
     </div>
   );
 }
