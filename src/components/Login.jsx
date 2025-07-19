941bce90e9e52f57d91bcbef4f543000bc7287bd
progaurav052 <gpprojects511@gmail.com>
2025-07-19 20:53:19 +0530

    Chnaged the Parameter being used in updateProfile

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -41,7 +41,7 @@ const Login = () => {
         .then((userCredential) => {
           // Signed up
           const user = userCredential.user;
-          updateProfile(user, {
+          updateProfile(auth.currentUser, {
             displayName: name.current.value,
             photoURL:
               "https://occ-0-2611-3663.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABeuqjuQsRgqEDlibtJTI5BMf8IxhLlLOeIT6xI4TL57mqv7XHja43gx02S8pZVe8JNGRQXjnrUk1VcsTXqi83tFKPI6OR3k.png?r=bd7",
